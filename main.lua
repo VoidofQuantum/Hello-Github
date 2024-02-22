8d653d2f93f629063370e1383aac9ae75a1facf0
Quantum <153005653+VoidofQuantum@users.noreply.github.com>
2024-02-22 19:35:33 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,3 +2,7 @@ local ip1 = math.random(1,255)
 local ip2 = math.random(1, 255)
 local ip3 = math.random(1,255)
 local ip4 = math.random(1, 255)
+
+print("Generated in 1.5 seconds")
+
+print("Ip is" .. ip1 .. ".".. ip2 .. "." .. ip3 .. "." .. ip4)
